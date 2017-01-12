3623cb62189976cc4257a364e1fdd7def932168b
rveciana <rveciana@gmail.com>
2017-01-12 15:17:57 +0100

    rollup and test set

---
+++ build/d3-labels.js
@@ -0,0 +1,16 @@
+// d3-labels Version 0.0.1. Copyright 2017 Roger Veciana i Rovira.
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+	typeof define === 'function' && define.amd ? define(['exports'], factory) :
+	(factory((global.d3 = global.d3 || {})));
+}(this, (function (exports) { 'use strict';
+
+var labels = function() {
+
+};
+
+exports.labels = labels;
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
