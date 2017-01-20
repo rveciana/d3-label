f4a1fa95ee87abd50579b721fc289e55db20893b
rveciana <rveciana@gmail.com>
2017-01-20 15:22:07 +0100

    changes in sprites

--- build/d3-labels.js
+++ build/d3-labels.js
@@ -20,8 +20,9 @@ function labelsSprite(context, label) {
   var w = context.measureText(label.text + "m").width ,
   h = label.size << 1;
   w = (w + 0x1f) >> 5 << 5;
-
-  context.translate(w >> 1, h >> 1);
+  console.info(context.measureText(label.text + "m"));
+  //context.translate(w >> 1, h >> 1);
+  context.translate(0, label.size);
   context.fillText(label.text, 0, 0);
   if (label.padding) {
     context.lineWidth = 2 * label.padding;
@@ -31,7 +32,35 @@ function labelsSprite(context, label) {
   context.canvas.pngStream().pipe(fs.createWriteStream("/tmp/test.png"));
   label.width = w;
   label.height = h;
-  return null;
+
+  var pixels = context.getImageData(0, 0, cw << 5, ch).data,
+  sprite = [];
+  // -----> if (!d.hasText) continue;
+  var w32 = w >> 5;
+  for (var i = 0; i < h * w32; i++) {sprite[i] = 0;}
+  var seen = 0,
+      seenRow = -1,
+      y = 0, x = 0;
+  var y0 = h >> 1;
+  var y1 = y0;
+
+  for (var j = 0; j < h; j++) {
+    for (var i = 0; i < w; i++) {
+      var k = w32 * j + (i >> 5),
+          m = pixels[((y + j) * (cw << 5) + (x + i)) << 2] ? 1 << (31 - (i % 32)) : 0;
+      sprite[k] |= m;
+      seen |= m;
+    }
+    if (seen) {seenRow = j;}
+    else {
+      y0++;
+      h--;
+      j--;
+      y++;
+    }
+  }
+  y1 = y0 + seenRow;
+  return sprite.slice(0, (y1 - y0) * w32);
 }
 
 var labels = function() {
--- test/sprites-test.js
+++ test/sprites-test.js
@@ -3,17 +3,17 @@ Canvas = require("canvas"),
 sprites = require("./output/sprites");
 
 tape("Point labels sprite test", function(test) {
-  /*
+
   var label = {text: "Hello",
   geometry: [10, 10],
   font: "serif",
   style: "normal",
   weight: "normal",
-  size: 20,
+  size: 200,
   padding: 1};
   var context = new Canvas(1, 1).getContext("2d");
+  var sprite = sprites.labelsSprite(context, label);
 
-  //labels.labelsSprite(context, label);
-  console.info(sprites);*/
+  console.info("SPRITES", sprite.length);
   test.end();
 });
